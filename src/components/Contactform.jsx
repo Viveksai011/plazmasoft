59f2174336ae26a01c57acd249e3bd2b81a05ced
Viveksai011 <viveksai011@gmail.com>
2025-08-18 19:20:09 +0530

    label fix

--- src/components/Contactform.jsx
+++ src/components/Contactform.jsx
@@ -1,5 +1,5 @@
 import { Button } from "@/components/ui/button";
-import { Label } from "@/components/ui/Label";
+import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 
